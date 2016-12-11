9f6928813a4c54d42c4c99429a467808f5646a4f
Kaj <kaaiman99@hotmail.com>
2016-12-11 16:34:52 +0100

    Rare commit

--- Assets/_Scripts/UpgradeController.cs
+++ Assets/_Scripts/UpgradeController.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class UpgradeController : MonoBehaviour 
 {
-	private ParticleScoreSystem score;
-	private Money moneyScript;
-	private bool buy1 = true;
-	[SerializeField]private int upgradePrice;
+    private ParticleScoreSystem score;
+    private Money moneyScript;
+    private bool buy1 = true;
+    [SerializeField]private int upgradePrice;
 
-	void Start () 
-	{
-		score = GameObject.FindObjectOfType<ParticleScoreSystem> ();
-		moneyScript = GetComponent<Money> ();
-	}
+    void Start () 
+    {
+        score = GameObject.FindObjectOfType<ParticleScoreSystem> ();
+        moneyScript = GetComponent<Money> ();
+    }
 
-	public void upgrade (int cost)
-	{
-		if (moneyScript.getMoney >= cost && buy1) 
-		{
-			moneyScript.spendMoney (cost);
-			score.scoreAmount += 2;
-			buy1 = false;
-		}
-	}
+    public void upgrade (int cost)
+    {
+        if (moneyScript.getMoney >= cost && buy1) 
+        {
+            moneyScript.spendMoney (cost);
+            score.scoreAmount += 2;
+            buy1 = false;
+        }
+    }
 }
--- Assets/_Scripts/UpgradeUI.cs
+++ Assets/_Scripts/UpgradeUI.cs
@@ -6,10 +6,11 @@ public class UpgradeUI : MonoBehaviour
 {
 	private UpgradeController upgrades;
 	[SerializeField]private Button upgrade;
+	[SerializeField]private int upgradePrice;
 
 	private void Start()
 	{
 		upgrades = GetComponent<UpgradeController> ();
-		upgrade.onClick.AddListener (delegate(){upgrades.upgrade(100);});
+		upgrade.onClick.AddListener (delegate(){upgrades.upgrade(upgradePrice);});
 	}
 }
