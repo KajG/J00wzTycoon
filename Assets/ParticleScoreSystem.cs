19b133e0b29130dc48f3b1e3349f302f2f39cd3a
Kaj <kaaiman99@hotmail.com>
2016-12-11 12:09:34 +0100

    Initial commit nummer 2

---
+++ Assets/ParticleScoreSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScoreSystem : MonoBehaviour {
+	[SerializeField]
+	private ParticleSystem particleSystem;
+	[SerializeField]
+	private int burstDelay;
+	[SerializeField]
+	private int burstAmount;
+
+	void Start () {
+		var em = particleSystem.emission;
+		em.enabled = true;
+		em.SetBursts (new ParticleSystem.Burst[]  { 
+			new ParticleSystem.Burst (burstDelay, 100)
+		});	
+
+	}
+	
+	void Update () {
+		
+	}
+	void AddPoint(){
+
+
+	}
+}
