e9553e0937993f372f8750d912735b1002cdbbb3
Kaj <kaaiman99@hotmail.com>
2016-12-11 15:25:02 +0100

    Upgrade Controller

---
+++ Assets/UpgradeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeController : MonoBehaviour {
+
+	public ParticleScoreSystem score;
+	int currentScore;
+	bool buy1 = true;
+
+	void Start () {
+		score = GetComponent<ParticleScoreSystem> ();
+		
+	}
+	
+	void Update () {
+		currentScore = score.score;
+		if (currentScore >= 100 && buy1) 
+		{
+			score.scoreAmount += 2;
+			buy1 = false;
+		}
+
+
+	}
+}
