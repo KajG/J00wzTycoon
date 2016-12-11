9fc721d95bd27dd284cd808a2b4df804b674b6e5
Danivdwerf <danivdwerf.com>
2016-12-11 12:41:06 +0100

    getting the font

---
+++ Assets/_Scripts/Money.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Money : MonoBehaviour 
+{
+	private enum currency{Reichsmark};
+	private float moneyValue{ get; set;}
+	public float getMoney{get{return moneyValue;}}
+
+
+	private void Start()
+	{
+		moneyValue = 100;
+	}
+
+	public void addMoney(float amount)
+	{
+		moneyValue += amount;
+	}
+
+	public void spendMoney(float amount)
+	{
+		moneyValue -= amount;
+	}
+}
