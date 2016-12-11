b8ae0977602c908a8cd2299e7e51dd32e0daa943
Danivdwerf <danivdwerf.com>
2016-12-11 12:41:43 +0100

    pull

--- Assets/ParticleScoreSystem.cs
+++ Assets/ParticleScoreSystem.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ParticleScoreSystem : MonoBehaviour {
-	[SerializeField]
+	//[SerializeField]
 	private ParticleSystem particleSystem;
 	[SerializeField]
 	private int burstDelay;
 	[SerializeField]
 	private int burstAmount;
+	[SerializeField]
+	private int score;
+	[SerializeField]
+	private int scoreAmount;
+	[SerializeField]
+	private int particleCount;
 
 	void Start () {
-		var em = particleSystem.emission;
-		em.enabled = true;
-		em.SetBursts (new ParticleSystem.Burst[]  { 
-			new ParticleSystem.Burst (burstDelay, 100)
-		});	
-
+		particleSystem = GetComponent<ParticleSystem> ();
 	}
-	
+
 	void Update () {
-		
+		//int particleCount = particleSystem.GetParticles(ParticleSystem.Particle[] oparticlees,;
 	}
-	void AddPoint(){
-
-
+	public void AddPoint(){
+		score += scoreAmount;
+		particleSystem.Emit (scoreAmount);
 	}
 }
