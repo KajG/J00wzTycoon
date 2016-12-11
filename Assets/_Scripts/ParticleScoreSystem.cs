d613b17689dd23758b29fcbc2e6a2c7d08bd03d0
Danivdwerf <danivdwerf.com>
2016-12-11 16:11:58 +0100

    srp upgrade

--- Assets/_Scripts/ParticleScoreSystem.cs
+++ Assets/_Scripts/ParticleScoreSystem.cs
@@ -2,32 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParticleScoreSystem : MonoBehaviour {
-	//[SerializeField]
+public class ParticleScoreSystem : MonoBehaviour 
+{
 	private ParticleSystem particleSystem;
-	[SerializeField]
-	private int burstDelay;
-	[SerializeField]
-	private int burstAmount;
-	[SerializeField]
-	public int score;
-	[SerializeField]
-	public int scoreAmount;
-	[SerializeField]
-	private int particleCount;
-	[SerializeField]
-	private int range;
-	private bool scoreAdded = false;
+	private Money moneyScript;
+	[SerializeField]private int burstDelay;
+	[SerializeField]private int burstAmount;
+	[SerializeField]public int scoreAmount;
+	[SerializeField]private int particleCount;
+	[SerializeField]private int range;
 
-	void Start () {
+	void Start () 
+	{
 		particleSystem = GetComponent<ParticleSystem> ();
+		moneyScript = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<Money> ();
 	}
 
-	void Update () {
-		//int particleCount = particleSystem.GetParticles(ParticleSystem.Particle[] oparticlees,;
-	}
-	public void AddPoint(){
-		score += scoreAmount;
+	private void AddPoint()
+	{
+		moneyScript.addMoney(scoreAmount);
 		particleSystem.Emit (scoreAmount);
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+			AddPoint ();
+		}
 	}
 }
---
+++ Assets/_Scripts/UpgradeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeController : MonoBehaviour 
+{
+	private ParticleScoreSystem score;
+	private Money moneyScript;
+	private bool buy1 = true;
+	[SerializeField]private int upgradePrice;
+
+	void Start () 
+	{
+		score = GameObject.FindObjectOfType<ParticleScoreSystem> ();
+		moneyScript = GetComponent<Money> ();
+	}
+
+	public void upgrade (int cost)
+	{
+		if (moneyScript.getMoney >= cost && buy1) 
+		{
+			moneyScript.spendMoney (cost);
+			score.scoreAmount += 2;
+			buy1 = false;
+		}
+	}
+}
